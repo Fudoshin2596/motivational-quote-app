fb42038929072ee0b9ea5c6e29feed2b8df4aa42
Joel Alexander <joelalexander@lyft.com>
2023-01-04 01:31:48 -0500

    remove helper

--- database/db.py
+++ database/db.py
@@ -15,14 +15,3 @@ def initiate_mongo_client(collection: str) -> Collection:
 # Connect to the MongoDB database
 quotes_collection = initiate_mongo_client("QUOTE_COLLECTION")
 user_collection = initiate_mongo_client("USER_COLLECTION")
-
-
-def quote_helper(quote) -> dict:
-    return {
-        "id": str(quote["_id"]),
-        "user_id": str(quote["user_id"]),
-        "author": quote["author"],
-        "quote": quote["quote"],
-        "category": quote["category"],
-        "created_at": quote["created_at"],
-    }
