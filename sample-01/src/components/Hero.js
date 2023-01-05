775df91d330541a97a0dd80edbbff7206c4e0110
Joel Alexander <joelalexander@lyft.com>
2023-01-04 21:10:57 -0500

    add frontend for auth

---
+++ sample-01/src/components/Hero.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+// import logo from "../assets/logo.svg";
+
+const Hero = () => (
+  <div className="text-center hero my-5">
+    {/*<img className="mb-3 app-logo" src={logo} alt="React logo" width="120" />*/}
+    <h1 className="mb-4">Log in or Sign Up today!</h1>
+
+    <p className="lead">
+        MotiMe sends motivational quotes to your mobile phone every day!
+    </p>
+  </div>
+);
+
+export default Hero;
